7a12055b2fb45730cb2ed3ae5c5a96fec0e191f6
Timmyswitch <timilehin.dahunsi@interswitchng.com>
2026-03-17 08:49:22 +0100

    feat: Category filter component

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,34 +1,35 @@
-import { useEffect, useState } from "react"
-import type { ProductType } from "../../types/types"
-import { fetchProducts } from "../../services/ProductService"
-import ProductCard from "./Ui/ProductCard"
-
+import { useEffect, useState } from "react";
+import type { ProductType } from "../../types/types";
+import { fetchProducts } from "../../services/ProductService";
+import ProductCard from "./Ui/ProductCard";
+import Category from "./Category/Category";
 
 export default function Products() {
+  const [products, setProducts] = useState<ProductType[]>([]);
+    const [selectedCategory, setSelectedCategory] = useState("all");
 
-    const [products, setProducts] = useState<ProductType[]>([])
 
-    useEffect(() => {
-        async function fetchProduct() {
-            const data = await fetchProducts();
-            setProducts(data)
-        }
+  useEffect(() => {
+    async function fetchProduct() {
+      const data = await fetchProducts();
+      setProducts(data);
+    }
 
-        fetchProduct()
-    }, [])
+    fetchProduct();
+  }, []);
 
-    return(
-        <div>
-            {products.map((product) => {
-                return(
-                    <div key={product.id}>
-                        <ProductCard 
-                            product={product}  />
-                    </div>
-                )
-            })
-        }
-        </div>
-        
-    )
+  return (
+    <div>
+      <div>
+        <Category onSelectCategory={setSelectedCategory} />
+      </div>
+      {products.map((product) => {
+        return (
+          <div key={product.id}>
+            <ProductCard product={product} />
+          </div>
+        );
+      })}
+    </div>
+  );
 }
